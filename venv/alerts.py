bb082aca2bed770f9077dff7ed9f670ea4d96cc0
psykoholik <psykoz@gmail.com>
2018-08-08 23:48:22 -0500

    .

--- venv/alerts.py
+++ venv/alerts.py
@@ -1,2 +1,14 @@
+import smtplib
+from email.mime.text import MIMEText
 from CWCryptoWatch.CWCryptoWatch import CWCryptoWatch
 
+
+msg = MIMEText("hello world")
+
+msg['Subject'] = "subject Hello World"
+msg['To'] = "cwilkerson@gmail.com"
+msg['From'] = "cryptowatch@syzygy.io"
+
+s = smtplib.SMTP('localhost')
+s.sendmail(msg['From'], msg['To'], msg.as_string())
+s.quit()
